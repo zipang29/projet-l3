389c43b15efb1e3461144c3037507817d0b3b856
Guillaume Claudic <guillaume.claudic@gmail.com>
2014-02-21 15:36:05 +0100

    Transfert des fichiers déjà existants

---
+++ dev/js/Level.js
@@ -0,0 +1,76 @@
+/**
+* Charge le niveau correspondant
+* @var int level Le numéro du niveau à charger
+*/
+function loadLevel(level) {
+	switch (level) {
+		case 1:
+			loadLevel1();
+		break;
+		case 2:
+		
+		break;
+		case 3:
+		
+		break;
+		case 4:
+		
+		break;
+		case 5:
+		
+		break;
+		default:
+			loadLevel1();
+		break;
+	}
+}
+
+/**
+* Charge les éléments du niveau 1 du jeu
+*/
+function loadLevel1() {
+	var canvas = document.getElementById("elevator");
+	var context = canvas.getContext("2d");
+	
+	//Montagne en arrière plan
+	var montagneFond = new Image();
+	montagneFond.src = 'img/decors/lvl1/montagneArriere.png';
+	
+	montagneFond.onload = function () {
+		context.drawImage(this, 0, 0);
+	};
+	//Montagne à l'avant plan
+	var montagneAvant = new Image();
+	montagneAvant.src = 'img/decors/lvl1/montagneAvant.png';
+	montagneAvant.onload = function() {
+		context.drawImage(this, 0, 0);
+	};
+}
+
+/**
+* Charge les éléments du niveau 2 du jeu
+*/
+function loadLevel2() {
+
+}
+
+/**
+* Charge les éléments du niveau 3 du jeu
+*/
+function loadLevel3() {
+
+}
+
+/**
+* Charge les éléments du niveau 4 du jeu
+*/
+function loadLevel4() {
+
+}
+
+/**
+* Charge les éléments du niveau 5 du jeu
+*/
+function loadLevel5() {
+
+}
