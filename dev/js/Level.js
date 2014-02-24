c80ad337460fd49b0184e69f77e1da7b5f481317
Guillaume Claudic <guillaume.claudic@gmail.com>
2014-02-24 10:31:07 +0100

    Correction du problème d'affichage des images dans le canvas sur firefox. Cela venait de la taille des images qui à priori était trop larges. J'ai donc réduit de 20000px à 15000px la largeur des images de fond. Quelques modifications également dans le code.

--- dev/js/Level.js
+++ dev/js/Level.js
@@ -30,21 +30,21 @@ function loadLevel(level) {
 */
 function loadLevel1() {
 	var canvas = document.getElementById("elevator");
-	var context = canvas.getContext("2d");
-	
-	//Montagne en arrière plan
-	var montagneFond = new Image();
-	montagneFond.src = 'img/decors/lvl1/montagneArriere.png';
-	
-	montagneFond.onload = function () {
-		context.drawImage(this, 0, 0);
-	};
-	//Montagne à l'avant plan
-	var montagneAvant = new Image();
-	montagneAvant.src = 'img/decors/lvl1/montagneAvant.png';
-	montagneAvant.onload = function() {
-		context.drawImage(this, 0, 0);
-	};
+    var context = canvas.getContext("2d");
+ 
+    // Montagne en arrière plan
+    var montagneFond = new Image();
+    montagneFond.onload = function () {
+        context.drawImage(this, 0, 0);
+        // Montagne à l'avant plan
+        var montagneAvant = new Image();
+        montagneAvant.onload = function() {
+            context.drawImage(this, 0, 0);
+        };
+        montagneAvant.src = 'img/decors/lvl1/montagneAvant.png';
+    };
+    // Toujours définir la source après ajout de l'écouteur
+    montagneFond.src = 'img/decors/lvl1/montagneArriere.png';
 }
 
 /**
