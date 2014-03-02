5a711d31135e03495385fb565e821bbc0caff104
Guillaume Claudic <guillaume.claudic@gmail.com>
2014-03-02 10:37:45 +0100

    Ajout des fichiers image pour l'animation du personnage (pour faire en sorte de pouvoir le faire marcher)

--- dev/js/Main.js
+++ dev/js/Main.js
@@ -27,7 +27,7 @@ function redraw() {
 * Fonction anonyme qui s'exécute (évènement) à la pression d'une touche du clavier
 */
 document.onkeydown = function(event) {
-	x = x - 1; //Permet de faire avancer les 2 images de fond de 1 pixel à chaque appuie sur une touche
+	x = x - 4; //Permet de faire avancer les 2 images de fond de 4 pixel à chaque appuie sur une touche
 	redraw();
 };
 
