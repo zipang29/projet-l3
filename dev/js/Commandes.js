2f4e3210ce5d3e1be7c3d1160b0844a55febe3af
Guillaume Claudic <guillaume.claudic@gmail.com>
2014-03-03 14:04:36 +0100

    Correction d'un bug sous firefox

--- dev/js/Commandes.js
+++ dev/js/Commandes.js
@@ -3,13 +3,13 @@
 */
 document.onkeydown = function(e) {
 	// Flèche droite : on avance
-	if (window.event.keyCode == 39) {
+	if (e.keyCode == 39) {
 		if (Math.abs(x - 10) < f1.width - canvas.width && Math.abs(x - 10) < f2.width - canvas.width) {
 			x -= 10; // Permet de faire avancer de 10 pixel à chaque appuie sur une touche
 		}
 	}
 	// Flèche gauche : on recule
-	if (window.event.keyCode == 37) {
+	if (e.keyCode == 37) {
 		if (x + 10 <= 0) {
 			x += 10;
 		}
@@ -22,7 +22,7 @@ document.onkeydown = function(e) {
 */
 document.onkeyup = function(e) {
 	// Si on relache la flèche droite
-	if (window.event.keyCode == 39 || window.event.keyCode == 37) {
+	if (e.keyCode == 39 || e.keyCode == 37) {
 		statutPersonnage = 0; // On affiche l'image du personnage qui ne marche pas
 		context.drawImage(p1, 50, canvas.height - p1.height - sol.height);
 	}
