5db6e64e1ed2abceeba04b8121449a4a956ced6a
Guillaume Claudic <guillaume.claudic@gmail.com>
2014-03-06 01:02:03 +0100

    Documentation de Bidon.js + ajout du prototype d'une méthode pour gérer les collisions avec les bidons

--- dev/js/Bidon.js
+++ dev/js/Bidon.js
@@ -10,6 +10,10 @@ function Bidon(x1, x2) {
 	this.currentPos = x1;
 	this.avance = true;
 
+	/**
+	* Permet de changer la position de l'énnemi. Cette méthode est faite pour être utilisée comme indiqué ci-après
+	* @exemple setInterval("monbidon.lancerAnnimation()", 10);
+	*/
 	this.lancerAnnimation = function() {
 		if (this.avance && this.currentPos < this.x2) {
 			this.currentPos += 1;
@@ -25,6 +29,9 @@ function Bidon(x1, x2) {
 		}
 	};
 	
+	/**
+	* Met à jour la position et l'image du bidon
+	*/
 	this.update = function() {
 		if (this.avance) {
 			context.drawImage(bidon2, x + this.currentPos, canvas.height - bidon1.height - sol.height);
@@ -32,5 +39,14 @@ function Bidon(x1, x2) {
 		else {
 			context.drawImage(bidon1, x + this.currentPos, canvas.height - bidon1.height - sol.height);
 		}
+	};
+	
+	/**
+	* Détecte les collisions entre le joueur et un bidon
+	* @Joueur joueur Un objet de type joueur
+	* @return true si il y a collision, false sinon
+	*/
+	this.collision = function(joueur) {
+	
 	};
 }
