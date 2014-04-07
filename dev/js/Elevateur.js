8a9eadd34d8ddd213b4c5a0f938040cd63469aa2
Guillaume Claudic <guillaume.claudic@gmail.com>
2014-04-07 14:21:53 +0200

    Documentation des fichiers Plateforme.js et Elevateur.js

--- dev/js/Elevateur.js
+++ dev/js/Elevateur.js
@@ -2,11 +2,22 @@ var imgElevateur;
 
 var listeElevateurs = new Array();
 
+/**
+* Initialise les images des élévateurs
+*/
 function initElevateur() {
 	imgPlateforme = new Image();
 	imgPlateforme.src = "img/objets/interactions/elevateur.png";
 }
 
-function addElevateur(positionStartX, positionStartY) {
+/**
+* Permet d'ajouter un élévateur.
+* @var x1 La position x de l'élévateur dans sa position initiale
+* @var y2 La position y de l'élévateur dans sa position initiale
+* @var x2 La position x d'arrivée de l'élévateur
+* @var y2 La position y d'arrivée de l'élévateur
+* @var direction Un boolean est attendu. <code>true</code> si l'animation est sur l'axe x, <code>false</code> si il est sur l'axe y
+*/
+function addElevateur(x1, y1, x2, y2, direction) {
 
 }
--- dev/js/Plateforme.js
+++ dev/js/Plateforme.js
@@ -2,13 +2,18 @@ var imgPlateforme;
 
 var listePlateforme = new Array();
 
+/**
+* Initialise les images pour les plateformes
+*/
 function initPlateforme() {
 	imgPlateforme = new Image();
 	imgPlateforme.src = "img/objets/interactions/plateforme.png";
 }
 
 /**
-* Ajoute une plateforme
+* Ajoute une plateforme au niveau
+* @var x La position x de la plateforme
+* @var y La position y de la plateforme
 */
 function addPlateforme(x, y) {
 	listePlateforme[listePlateforme.length] = new Array();
@@ -17,6 +22,9 @@ function addPlateforme(x, y) {
 	plateforme['y'] = y;
 }
 
+/**
+* Permet d'afficher toutes les plateformes
+*/
 function displayAllPlateformes() {
 	var plateforme;
 	for (var i=0; i<listePlateforme.length; i++) {
